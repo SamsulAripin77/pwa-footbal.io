c90abe38d039e9114723b8e6f894ce5d1566fd43
SamsulAripin77 <arifins192@gmail.com>
2020-10-19 12:26:08 +0700

    syarat crud submission ok

--- asset/js/api.js
+++ asset/js/api.js
@@ -1,40 +1,200 @@
-const baseUrl = "http://api.football-data.org/v2/teams"
-const tempUrl = "/data/";
-
-
-function status (response) {
-    if (response.status !== 200) {
-        console.log('Error : ' + response.status);
-        // Method reject() akan membuat blok catch terpanggil
-        return Promise.reject(new Error(response.statusText));
-    } else {
-        // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
-        return Promise.resolve(response);
-    }
-}
-
-function json (response) {
-    // Mengembalikan sebuah Promise berupa objek/array JavaScript
-    // yang diubah dari teks JSON. 
-    return response.json();
-}
-function error (error) {
-    // Parameter error berasal dari Promise.reject() 
-    console.log('Error : ' + error);
-}
-
-fetch(`${tempUrl}team.json`,{
-    method: "GET",
+var baseUrl = './data/team.json';
+var apiUrl = 'http://api.football-data.org/v2/teams';
+var apiOption = {
+    method: 'GET',
+    mode: 'no-cors',
     withCredentials: true,
-    'headers':{
+    headers: {
         'Content-Type': 'application/json',
         'X-Auth-Token': '4d8cdc4b83c94ba19969ac6990d91775',
-        "Content-Type": "application/x-www-form-urlencoded"
+        // 'Access-Control-Allow-Headers': '*'
     }
-})
-    .then(status)
-    .then(json)
-    .then(function(data) {
-        // Objek/array JavaScript dari response.json() masuk lewat data.
-        console.log(data);
-    }).catch(error);
+}
+
+
+function cardItem(team) {
+    console.log(team)
+    return `
+  <div class="col l3 m4 s12">
+   <div class="card">
+    <a href="./detail.html?id=${team.id}">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img src="../team.svg" alt="team.svg"/>
+      </div>
+    </a>
+    <div class="card-content">
+      <span class="card-title truncate">${team.name}</span>
+    </div>
+  </div>
+  </div>
+  `;
+}
+
+function cardSavedtem(team) {
+    return `
+  <div class="col l3 m4 s12">
+   <div class="card">
+    <a href="./detail.html?id=${team.id}&saved=true">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img src="../team.svg" alt="team.svg"/>
+      </div>
+    </a>
+    <div class="card-content">
+      <span class="card-title truncate">${team.name}</span>
+    </div>
+  </div>
+  </div>
+  `;
+}
+
+function cardDetailsTeam(team) {
+    return `
+        <li class=""><a href="./detail.html">${team.name}</a></li>
+  `
+}
+
+function cardDetailSavedTeam(team) {
+    return `
+        <li class=""><a href="./detail.html">apa aja</a></li>
+  `
+}
+
+function status(response) {
+    if (response.status !== 200) {
+        // Method reject() akan membuat blok catch terpanggil
+        console.log('gagal parsing api')
+    }
+    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+    return Promise.resolve(response);
+}
+
+function json(response) {
+    return response.json();
+}
+
+function error(err) {
+    console.log(`Error : ${err}`);
+}
+
+function hideSaveButton(id) {
+    let arryId = [];
+    let btnSaved = document.querySelector('#save');
+    getAll().then((teams) => {
+        teams.forEach((team) => {
+            arryId.push(team.id)
+        });
+        if (arryId.includes(parseInt(id))) {
+            btnSaved.style.display = 'none';
+        }
+    });
+}
+
+function getTeam() {
+    if ('caches' in window) {
+        caches.match(`${baseUrl}team.json`).then((response) => {
+            if (response) {
+                response.json().then((data) => {
+                    let content = '';
+                    data.teams.forEach((team) => {
+                        content += cardItem(team);
+                    });
+                    document.querySelector('#body-content').innerHTML = content;
+                });
+            }
+        });
+    }
+
+    fetch(`${baseUrl}`, {
+            headers: {
+                'X-Auth-Token': '4d8cdc4b83c94ba19969ac6990d91775'
+            }
+        })
+        .then(status)
+        .then(json)
+        .then((data) => {
+            let content = '';
+            data.teams.forEach((team) => {
+                content += cardItem(team);
+            });
+            document.querySelector('#body-content').innerHTML = content;
+        })
+        .catch(error);
+}
+
+function getTeamById() {
+    return new Promise((resolve, reject) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const idParam = urlParams.get('id');
+        console.log(idParam);
+        let content = '';
+        if ('caches' in window) {
+            caches.match('oneTeam.json').then((response) => {
+                if (response) {
+                    response.json().then((data) => {
+                        content = cardDetailsTeam(data);
+                        document.querySelector('#detail-content').innerHTML = content;
+                        resolve(data);
+                    });
+                }
+            });
+        }
+        fetch(`${apiUrl}/${idParam}`, {
+                headers: {
+                    'X-Auth-Token': '4d8cdc4b83c94ba19969ac6990d91775'
+                }
+            })
+            .then((response) => {
+                return response;
+            })
+            .then(json)
+            .then((data) => {
+                content = cardDetailsTeam(data);
+                console.log(data)
+                let detail = document.querySelector('#detail-content') || '';
+                detail.innerHTML = content;
+                resolve(data)
+            })
+            .then(hideSaveButton(idParam))
+            .catch(err => console.log(err))
+    });
+}
+
+function getSavedTeams() {
+    getAll().then((teams) => {
+        console.log(teams);
+        let content = '';
+        var arryId = [];
+        teams.forEach((team) => {
+            console.log(team.id);
+            arryId.push(team.id)
+            content += cardSavedtem(team);
+        });
+        console.log(arryId);
+        if (arryId.includes(63)) {
+            console.log('ada')
+        }
+        let saved = document.querySelector('#body-content');
+        saved.innerHTML = content;
+    });
+}
+
+function getSavedTeamById() {
+    return new Promise((resolve, reject) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const idParam = parseInt(urlParams.get('id'));
+        console.log(idParam)
+        let content = 'hallo';
+        getById(idParam).then((team) => {
+            console.log(team)
+            content = cardDetailsTeam(team);
+            console.log(content)
+            let detail = document.querySelector('#detail-content');
+            detail.innerHTML = content;
+            resolve(team)
+        })
+    })
+}
+
+function contoh() {
+    document.querySelector('#apa').innerHTML = 'selamat'
+}
--- asset/js/nav.js
+++ asset/js/nav.js
@@ -1,68 +1,66 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Activate sidebar nav
-  var elems = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(elems);
-  loadNav();
+document.addEventListener('DOMContentLoaded', () => {
+    // Activate sidebar nav
+    const elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+    loadNav();
 
-  function loadNav() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status != 200) return;
+    function loadNav() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status != 200) return;
 
-        // Muat daftar tautan menu
-        document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
-          elm.innerHTML = xhttp.responseText;
-        });
+                // Muat daftar tautan menu
+                document.querySelectorAll('.topnav, .sidenav').forEach((elm) => {
+                    elm.innerHTML = xhttp.responseText;
+                });
 
-        // Daftarkan event listener untuk setiap tautan menu
-        document
-          .querySelectorAll(".sidenav a, .topnav a")
-          .forEach(function(elm) {
-            elm.addEventListener("click", function(event) {
-              // Tutup sidenav
-              var sidenav = document.querySelector(".sidenav");
-              M.Sidenav.getInstance(sidenav).close();
+                // Daftarkan event listener untuk setiap tautan menu
+                document.querySelectorAll('.sidenav a, .topnav a').forEach((elm) => {
+                    elm.addEventListener('click', (event) => {
+                        // Tutup sidenav
+                        const sidenav = document.querySelector('.sidenav');
+                        M.Sidenav.getInstance(sidenav).close();
 
-              // Muat konten halaman yang dipanggil
-              page = event.target.getAttribute("href").substr(1);
-              loadPage(page);
-            });
-          });
-      }
-    };
-    xhttp.open("GET", "nav.html", true);
-    xhttp.send();
-  }
+                        // Muat konten halaman yang dipanggil
+                        page = event.target.getAttribute('href').substr(1);
+                        loadPage(page);
+                    });
+                });
+            }
+        };
+        xhttp.open('GET', 'nav.html', true);
+        xhttp.send();
+    }
 
-  // Load page content
-  var page = window.location.hash.substr(1);
-  if (page == "") page = "home";
-  loadPage(page);
+    // Load page content
+    let page = window.location.hash.substr(1);
+    if (page == '') page = 'home';
+    loadPage(page);
 
-  function loadPage(page) {
-    // fetch('pages/' + page + '.html')
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        var content = document.querySelector("#content-body");
-        
-        if (page === "home") {
-          // getArticles();
-        } else if (page === "saved") {
-          // getSavedArticles();
-        }
+    function loadPage(page) {
+        // fetch('pages/' + page + '.html')
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                const content = document.querySelector('#main-content') || '';
 
-        if (this.status == 200) {
-          content.innerHTML = xhttp.responseText;
-        } else if (this.status == 404) {
-          content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-        } else {
-          content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-        }
-      }
-    };
-    xhttp.open("GET", "pages/" + page + ".html", true);
-    xhttp.send();
-  }
+                if (page === 'home') {
+                    getTeam();
+                } else if (page === 'saved') {
+                    getSavedTeams()
+                }
+
+                if (this.status == 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status == 404) {
+                    content.innerHTML = '<p>Halaman tidak ditemukan.</p>';
+                } else {
+                    content.innerHTML = '<p>Ups.. halaman tidak dapat diakses.</p>';
+                }
+            }
+        };
+        xhttp.open('GET', `pages/${page}.html`, true);
+        xhttp.send();
+    }
 });
